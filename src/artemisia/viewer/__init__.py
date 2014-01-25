7295f7c25e4537df8968c4996b2219ea78a3681b
Fräntz Miccoli <frantz.miccoli@fakedomain.com>
2014-01-26 00:59:26 +0100

    The viewer is still messy but it can handles color in stack charts

--- src/artemisia/viewer/__init__.py
+++ src/artemisia/viewer/__init__.py
@@ -1,17 +1,10 @@
-import matplotlib
-import artemisia.aggregator as gaggregator
-import artemisia.helper as ghelper
-
-matplotlib.use('Agg')  # works better in Vagrant
-import pylab
-try:
-    import seaborn
-except ImportError:
-    print 'seaborn is not available, the final result could be more pretty'
-
 from itertools import cycle
 import types
 import numpy
+
+from matplotlib_wrapper import MatplotlibWrapper
+import artemisia.aggregator as gaggregator
+import artemisia.helper as ghelper
 
 
 class Viewer:
@@ -23,36 +16,47 @@ class Viewer:
 
     def __init__(self):
         self._helper = ghelper.Helper()
+        self._matplotlib_wrapper = None
+        self._init_attributes()
 
     def plot(self, *args, **kwargs):
         args = list(args)
         data_generator = args.pop(0)
-        axis_columns = args
+        self._axis_columns = args
         self._color_column = kwargs.get('color_column', None)
-        if (self._color_column is not None) & (self._color_column not in axis_columns):
-            axis_columns.append(self._color_column)
+        if (self._color_column is not None) \
+                & (self._color_column not in self._axis_columns):
+            self._axis_columns.append(self._color_column)
         self._output_file_name = kwargs.get('output_file_name', 'out.png')
         self._y_label = kwargs.get('y_label', None)
         self._scatter = kwargs.get('scatter', False)
-        if ((self._color_column is not None) & (len(axis_columns) > 3))\
-            | ((self._color_column is None) & (len(axis_columns) > 2)):
+        if ((self._color_column is not None) & (len(self._axis_columns) > 3))\
+                | ((self._color_column is None)
+                    & (len(self._axis_columns) > 2)):
             raise Exception("Unexpected axis count")
 
-        xy_axis_columns = list(axis_columns[:])
+        self._xy_axis_columns = list(self._axis_columns[:])
         if (not self._scatter) & (self._color_column is not None):
-            xy_axis_columns.remove(self._color_column)
-        if len(xy_axis_columns) < 2:
-            xy_axis_columns.append('aggregate')
+            self._xy_axis_columns.remove(self._color_column)
+        if len(self._xy_axis_columns) < 2:
+            self._xy_axis_columns.append('aggregate')
 
-        aggregate = self._get_aggregate(data_generator, axis_columns)
-        plot_ready_values = self._get_plot_ready_values(aggregate, xy_axis_columns)
+        aggregate = self._get_aggregate(data_generator, self._axis_columns)
+        plot_ready_values = self._get_plot_ready_values(aggregate,
+                                                        self._xy_axis_columns)
 
-        self._plot(plot_ready_values, xy_axis_columns)
+        self._plot(plot_ready_values)
 
-        #avoid big margins (should be sent as an argument
-        pylab.tight_layout()
+    def _init_attributes(self):
+        self._color_column = None
+        self._output_file_name = None
+        self._y_label = None
+        self._scatter = None
+        self._axis_columns = None
+        self._xy_axis_columns = None
+        self._aggregate = None
+        self._plot_ready_values = None
 
-        pylab.savefig(self._output_file_name)
 
     def _get_aggregate(self, data_generator, axis_columns):
         aggregator = gaggregator.Aggregator()
@@ -81,7 +85,7 @@ class Viewer:
             y_values.append(value_point[xy_axis_columns[1]])
         return prepared_data
 
-    def _plot(self, plot_ready_values, xy_axis_columns):
+    def _plot(self, plot_ready_values):
         self._x_values_set = []
         colors = cycle("rgcmykb")
         x_axis_is_string = False
@@ -100,36 +104,59 @@ class Viewer:
                 x_axis_is_string = True
                 bar_width = 0.5
                 if self._scatter:
-                    self._x_values_set += list(set(x) - set(self._x_values_set))
-                    indexes = map(lambda x_value: self._x_values_set.index(x_value), x)
+                    self._x_values_set += list(set(x)
+                                               - set(self._x_values_set))
+                    indexes = map(lambda x_value:
+                                  self._x_values_set.index(x_value), x)
                     marker = color + "."
-                    pylab.plot(indexes, y, marker, label=color_column_value)
+                    self._get_matplotlib_wrapper()\
+                        .plot(indexes, y, marker,label=color_column_value)
                 else:
+                    color_pushed = False
                     for x_value in x:
                         index = x.index(x_value)
-                        pylab.bar(index, y[index], bar_width, color=color,
-                          label=x_value)
-                        pylab.xticks(numpy.arange(len(x)) + (bar_width / 2), x)
+                        if not color_pushed:
+                            label = color_column_value
+                            color_pushed = True
+                        else:
+                            label = None
+                        self._get_matplotlib_wrapper()\
+                            .bar(index, y[index],bar_width, color=color,
+                                 label=label)
+                        self._get_matplotlib_wrapper()\
+                            .xticks(numpy.arange(len(x)) + (bar_width / 2), x)
             else:
                 marker = color + "."
-                pylab.plot(x, y, marker, label=color_column_value)
+                self._get_matplotlib_wrapper()\
+                    .plot(x, y, marker, label=color_column_value)
 
         legend = None
         if x_axis_is_string:
-            pylab.xlabel(xy_axis_columns[0])
-            pylab.ylabel(xy_axis_columns[1])
+            self._get_matplotlib_wrapper().xlabel(self._xy_axis_columns[0])
+            self._get_matplotlib_wrapper().ylabel(self._xy_axis_columns[1])
             if self._scatter:
-                pylab.xlim([-0.1, len(self._x_values_set) - 0.9])
-                legend = pylab.legend(loc='lower right', numpoints=1)
+                self._get_matplotlib_wrapper()\
+                    .xlim([-0.1, len(self._x_values_set) - 0.9])
         else:
-            pylab.xlabel(xy_axis_columns[0])
+            self._get_matplotlib_wrapper().xlabel(self._xy_axis_columns[0])
             if self._y_label is None:
-                self._y_label = xy_axis_columns[1]
-            pylab.ylabel(self._y_label)
-            legend = pylab.legend(loc='lower right', numpoints=1)
+                self._y_label = self._xy_axis_columns[1]
+            self._get_matplotlib_wrapper().ylabel(self._y_label)
+
+        if self._color_column is not None:
+            legend = self._get_matplotlib_wrapper() \
+                .legend(loc='lower right', numpoints=1)
 
         if legend is not None:
             legend.get_frame().set_alpha(0.7)
 
-        pylab.subplots_adjust(left=0.15)
+        self._get_matplotlib_wrapper().subplots_adjust(left=0.15)
 
+        #avoid big margins (should be sent as an argument
+        self._get_matplotlib_wrapper().tight_layout()
+        self._get_matplotlib_wrapper().savefig(self._output_file_name)
+
+    def _get_matplotlib_wrapper(self):
+        if self._matplotlib_wrapper is None:
+            self._matplotlib_wrapper = MatplotlibWrapper()
+        return self._matplotlib_wrapper
--- src/artemisia/viewer/matplotlib_wrapper.py
+++ src/artemisia/viewer/matplotlib_wrapper.py
@@ -5,7 +5,16 @@ try:
     import seaborn
 except ImportError:
     print 'seaborn is not available, the final result could be more pretty'
+import types
 
 
 class MatplotlibWrapper:
+
+    def __getattr__(self, name, *args, **kwargs):
+        attr = getattr(pylab, name)
+        if isinstance(attr, types.FunctionType):
+            def wrapped_function(*args, **kwargs):
+                return attr(*args, **kwargs)
+            return wrapped_function
+        return attr
 
